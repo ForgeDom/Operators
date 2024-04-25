eff238ae00598cc6a9c2f121aec6e1495cee3c5d
ForgeDom <maxstill2006@gmail.com>
2024-04-25 18:51:19 +0300

    Lab ex2 done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main() {
-
-
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int grn, cop;
+	cout << "Введіть суму в гривнях та копійках: " << endl;
+	cin >> grn >> cop;
+	grn += cop / 100;
+	cop %= 100;
+	cout << grn << "грн. " << cop << "коп.";
 	return 0;
 }
