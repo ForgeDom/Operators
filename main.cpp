10a88b7a189d8ccfa306a2b088d4685ef7792867
ForgeDom <maxstill2006@gmail.com>
2024-04-25 19:09:31 +0300

    Lab ex5 done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
+	const double p = 3.14;
+	double r;
+	cout << "«находженн€ об'Їму кул≥: " << endl;
+	cout << "¬вед≥ть R(рад≥ус): " << endl;
+	cin >> r;
 
+	double v = (4 * p * pow(r, 3) / 3);
+
+	cout << "ќб'Їм кул≥ - " << v << "см/куб.";
 	return 0;
 }
