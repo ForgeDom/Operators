302a961a008e0c8d13d2ff381f5e179c41e1fcab
ForgeDom <maxstill2006@gmail.com>
2024-04-25 19:18:45 +0300

    Hw ex1 done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
+    int sec;
+    cout << "Введіть кількість секунд: ";
+    cin >> sec;
+
+    int h = sec / 3600;
+    int m = (sec % 3600) / 60;
+    int s = sec % 3600;
+
+    cout << "Годинни: " << h << " Хвилини: " << m << " Секунди: " << s;
 
 	return 0;
 }
