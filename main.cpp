c92c10ccc7ea14ef2f309a70f5bd8c7dff9e9f8b
ForgeDom <maxstill2006@gmail.com>
2024-04-25 19:24:36 +0300

    hw ex2 done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
+	double drib;
+	cout << "" << endl;
+	cin >> drib;
+
+	double grn = static_cast<int>(drib);
+	double cop = static_cast<int>((drib - grn) * 100);
+	cout << grn << " грн." << cop << " коп.";
 
 	return 0;
 }
