a9501a6084201d636135a1cd881425e7842232f5
ForgeDom <maxstill2006@gmail.com>
2024-04-25 19:52:12 +0300

    Ex4 hw done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
+    int total_days;
+    cout << "¬вед≥ть к≥льк≥сть дн≥в: " << endl;
+    cin >> total_days;
 
+    int days = total_days % 7;
+    int weeks = total_days / 7;
+   
+    cout << weeks << " тижн≥в " << days << " дн≥в " << endl;
 	return 0;
 }
