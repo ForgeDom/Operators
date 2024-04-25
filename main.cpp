37eb4b3d8f5528a0aba9d920a4e4baa01ff982e1
ForgeDom <maxstill2006@gmail.com>
2024-04-25 18:43:01 +0300

    Lab ex1 done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main() {
+    SetConsoleCP(1251); 
+    SetConsoleOutputCP(1251);
 
+    int year;
+    cout << "Введіть номер року: ";
+    cin >> year;
 
-	return 0;
+    int days = 365; 
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        days = 366;
+    }
+
+    cout << "У " << year << " році " << days << " днів." << endl;
+
+    return 0;
+	
 }
