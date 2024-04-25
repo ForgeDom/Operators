4cc1b5514e5a66bbc3edbb2a2a8e8937b3944115
ForgeDom <maxstill2006@gmail.com>
2024-04-25 18:55:28 +0300

    Lab ex3 done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
+    cout << "Обчислення об'єму паралелепіпеда." << endl;
+    cout << "Введіть вихідні дані:" << endl;
+
+    double length, width, height;
+
+    cout << "Довжина:";
+    cin >> length;
+    cout << "Ширина:";
+    cin >> width;
+    cout << "Висота:";
+    cin >> height;
+
+    double v = length * width * height;
+        
+    cout << "Об'єм: " << v << " куб.см." << endl;
 
 	return 0;
 }
