a1d7a2b26b44395e219078dc23d62b91ae296343
ForgeDom <maxstill2006@gmail.com>
2024-04-25 19:01:38 +0300

    Lab ex4 done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
+	double mash, way;
 
+	cout << "Обчислення відстані між населеними пунктами." << endl;
+	cout << "Введіть вихідні дані:" << endl;
+
+	cout << "Масштаб карти (кількість кілометрів в одному сантиметрі): ";
+	cin >> mash;
+	cout << "Відстань між точками, що зображують населені пункти (см): ";
+	cin >> way;
+
+	double f_way = way / 100 * mash;
+
+	cout << "Відстань між населеними пунктами " << f_way << " км." << endl;
 	return 0;
 }
